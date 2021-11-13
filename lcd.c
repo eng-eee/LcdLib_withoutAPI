7a76dd6f7609f653d12adad1ceb3caa5bb9558ce
eng-eee <86379996+eng-eee@users.noreply.github.com>
2021-11-13 14:50:45 +0300

    Update lcd.c

--- lcd.c
+++ lcd.c
@@ -6,54 +6,61 @@
  */
 #include "lcd.h"
 
+void Delay(uint32_t second){
+    NVIC_ST_RELOAD_R=second;
+    NVIC_ST_CURRENT_R=0X00;
+    NVIC_ST_CTRL_R |= NVIC_ST_CTRL_CLK_SRC|NVIC_ST_CTRL_ENABLE;  //16mhz clock source
+    while((NVIC_ST_CTRL_R & NVIC_ST_CTRL_COUNT)==0);
+    NVIC_ST_CTRL_R &= ~NVIC_ST_CTRL_ENABLE;
+}
 void LcdEnable(){
     GPIO_PORTB_DATA_R |= 0x02; // EN=1
-    SysCtlDelay(100);
+    Delay(DELAY_100us);
     GPIO_PORTB_DATA_R &= 0XFC ; // EN=0
 }
 void LcdCmd(unsigned char c){
     GPIO_PORTB_DATA =(c & 0xf0);
     GPIO_PORTB_CONTROL=0X00; //0X00  ~(1<<RS)
     LcdEnable();
-    SysCtlDelay(50000);
+    Delay(DELAY_4_1ms);
 
     GPIO_PORTB_DATA =((c & 0x0f)<<4);
     GPIO_PORTB_CONTROL = 0X00; //~(1<<RS);
     LcdEnable();
-    SysCtlDelay(50000);
+    Delay(DELAY_4_1ms);
 }
 void LcdWrite(unsigned char c){
     GPIO_PORTB_DATA =(c & 0xf0); //D4 D5 D6 D7 -> HIGH NIBBLE
     GPIO_PORTB_CONTROL=0X01; //0X00  ~(1<<RS)
     LcdEnable();
-    SysCtlDelay(50000);
+    Delay(DELAY_4_1ms);
 
     GPIO_PORTB_DATA =((c & 0x0f)<<4); //D4 D5 D6 D7 -> LOW NIBBLE
     GPIO_PORTB_CONTROL = 0X01; //~(1<<RS);
     LcdEnable();
-    SysCtlDelay(50000);
+    Delay(DELAY_4_1ms);
 }
 void LcdHome(){
     LcdCmd(0x02);
-    SysCtlDelay(1000);
+    Delay(DELAY_100us);
 }
 void LcdClear(){
     LcdCmd(0x01);
     LcdCmd(0x02);
-    SysCtlDelay(1000);
+    Delay(DELAY_100us);
 }
 void LcdDisplayOn(){
     LcdCmd(0x0c);
-    SysCtlDelay(1000);
+    Delay(DELAY_100us);
 }
 
 void LcdCursorOn(){
     LcdCmd(0x0e);
-    SysCtlDelay(1000);
+    Delay(DELAY_100us);
 }
 void LcdBlinkOn(){
     LcdCmd(0x0f);
-    SysCtlDelay(1000);
+    Delay(DELAY_100us);
 }
 void LcdSetCursor(unsigned char row ,unsigned char col){
     char adress;
@@ -82,13 +89,13 @@ void LcdInit(){
 
     GPIO_PORTB_DATA=0X30; //(1<<4)|(1<<5); //30H
     LcdEnable();
-    SysCtlDelay(50000);
+    Delay(DELAY_15ms);
     GPIO_PORTB_DATA=0X30;
     LcdEnable();
-    SysCtlDelay(50000);
+    Delay(DELAY_4_1ms);
     GPIO_PORTB_DATA =0X30;
     LcdEnable();
-    SysCtlDelay(50000);
+    Delay(DELAY_4_1ms);
 
     LcdCmd(0x3F);
     LcdCmd(0x0e); // DISPALY ON CURSON ON
